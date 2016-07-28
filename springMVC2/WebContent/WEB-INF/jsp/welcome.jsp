6b26c496745c7abc1776f165220e5d86f76de6e2
allon2005 <allon2005@gmail.com>
2016-07-27 23:24:16 -0500

    1st MVC

---
+++ springMVC2/WebContent/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>SpringMVC1 Example</title>
+
+</head>
+<body>
+
+${message}
+
+</body>
+</html>
---
+++ springMVC2/src/springMVC2Controller/HelloWorld.java
@@ -0,0 +1,20 @@
+package springMVC2Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+//@RequestMapping("/welcome")
+//@RequestMapping("/")
+public class HelloWorld {
+	
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld()
+	{
+		String message = "<br><div stylr='text-align:center;'>"
+				+ "<h3> ****Hello world!</h3>";
+		return new ModelAndView("welcome","message",message); //this will return the model (name is "message") with view name "welcome" ==>welcome.jsp
+	}
+
+}
