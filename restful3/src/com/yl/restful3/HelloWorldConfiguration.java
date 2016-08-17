9c7efd4fd8cc1f06235dcfbd743e432ecf09b9bc
allon2005 <allon2005@gmail.com>
2016-08-16 21:27:30 -0500

    restful API 3#, @RestController example

---
+++ restful3/src/com/yl/restful3/HelloWorldConfiguration.java
@@ -0,0 +1,13 @@
+package com.yl.restful3;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+ 
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.yl.restful3")
+public class HelloWorldConfiguration {
+     
+ 
+}
